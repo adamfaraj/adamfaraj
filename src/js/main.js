3758c6c3da9f2ab61dd759c32ab7cbd0068fc513
Adam Faraj <adamf@marketamerica.com>
2020-07-20 12:52:46 -0400

    fix things and make prettier

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 (function($) {
 
   let dom;
+  // TODO: add navbar show/hide functionality
+  // let lastScrollPosition;
+  // let timer = null;
 
   const init = function() {
     cacheDOM();
@@ -13,11 +16,26 @@
     dom.title = dom.document.find(".about__title");
     dom.skillsLink = dom.document.find('.about__skill-link');
     dom.skills = dom.document.find('.skills__header');
+    // dom.navbar = dom.document.find('.navbar');
   }
 
   const bindEventHandlers = function() {
     dom.title[0].addEventListener("load", pageLoad());
     dom.skillsLink[0].addEventListener("click", skillClick);
+
+    // window.addEventListener('scroll', function(e) {
+    //   console.log('lastScrollPosition', lastScrollPosition);
+    //   lastScrollPosition = e.target.scrollingElement.scrollTop;
+    //   console.log('currentScrollPosition', e.target.scrollingElement.scrollTop);
+
+    //   if ((lastScrollPosition == undefined) || lastScrollPosition < e.target.scrollingElement.scrollTop) {
+    //     dom.navbar[0].style.marginTop = "-56px";
+    //     console.log('scroll down');
+    //   } else {
+    //     dom.navbar[0].style.marginTop = "0";
+    //     console.log('scroll up');
+    //   }
+    // })
   }
 
   const pageLoad = function() {
